62f1adbbb5d6cfb5da194c8b029a472ba425e716
mohamed-amine-mighri <mghmed.amine@gmail.com>
2023-11-14 00:09:04 +0100

    fixing sonar

--- src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
+++ src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
@@ -1,13 +1,17 @@
 package com.lovetolearn.SpringBootApp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootAppApplicationTests {
 
+	@Autowired
+	private SpringBootAppApplication springBootAppApplication;
 	@Test
 	void contextLoads() {
+		assert(springBootAppApplication != null);
 	}
 
 }
