25919114520073c4ba4330e6a6225d05d742a2d0
mohamed-amine-mighri <mghmed.amine@gmail.com>
2023-11-14 00:22:26 +0100

    fixing sonar

--- src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
+++ src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SpringBootAppApplicationTests {
+public class SpringBootAppApplicationTests {
 
 	@LocalServerPort
 	private int port;
@@ -24,5 +27,10 @@ class SpringBootAppApplicationTests {
 		String url = "http://localhost:" + port + "/";
 		String response = restTemplate.getForObject(url, String.class);
 		assertThat(response).contains("Hello");
+	}
+
+	@Configuration
+	@EnableWebMvc
+	static class TestConfig extends WebMvcConfigurerAdapter {
 	}
 }
