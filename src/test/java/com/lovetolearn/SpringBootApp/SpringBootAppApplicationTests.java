71877710f282438ee03e60d41f3483d541948bf6
mohamed-amine-mighri <mghmed.amine@gmail.com>
2023-11-14 00:19:19 +0100

    fixing sonar

--- src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
+++ src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
@@ -1,17 +1,28 @@
 package com.lovetolearn.SpringBootApp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootAppApplicationTests {
 
-	@Autowired
-	private SpringBootAppApplication springBootAppApplication;
+	@LocalServerPort
+	private int port;
+
+	private final TestRestTemplate restTemplate = new TestRestTemplate();
+
 	@Test
-	void contextLoads() {
-		assert(springBootAppApplication != null);
+	public void contextLoads() {
+		// Check that the Spring context loads successfully
 	}
 
+	@Test
+	public void homeEndpointReturnsHello() {
+		String url = "http://localhost:" + port + "/";
+		String response = restTemplate.getForObject(url, String.class);
+		assertThat(response).contains("Hello");
+	}
 }
