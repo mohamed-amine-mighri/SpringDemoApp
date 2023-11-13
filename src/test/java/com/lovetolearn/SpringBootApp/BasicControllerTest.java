8dec2f0c9cdcf10c323ffd7278664b213295e6e5
mohamed-amine-mighri <mghmed.amine@gmail.com>
2023-11-14 00:15:56 +0100

    fixing sonar

---
+++ src/test/java/com/lovetolearn/SpringBootApp/BasicControllerTest.java
@@ -0,0 +1,24 @@
+package com.lovetolearn.SpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BasicControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetData() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/get"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Welcome To My Test App"));
+    }
+}
