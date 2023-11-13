0b68e1705d32716df490e98c03c0d90f19c2bb02
mohamed-amine-mighri <mghmed.amine@gmail.com>
2023-11-14 00:45:23 +0100

    fixing sonar*

--- src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
+++ src/test/java/com/lovetolearn/SpringBootApp/SpringBootAppApplicationTests.java
@@ -2,29 +2,28 @@ package com.lovetolearn.SpringBootApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringBootAppApplicationTests {
-	@Autowired
-	private SpringBootAppApplication springBootAppApplication;
 
-	private final TestRestTemplate restTemplate = new TestRestTemplate();
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() {
-		// Check that the Spring context loads successfully
-		assert(springBootAppApplication != null);
-
+	public void contextLoads() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 	@Test
-	public void homeEndpointReturnsHello() {
-		String url = "http://localhost:" + 8080 + "/";
-		String response = restTemplate.getForObject(url, String.class);
-		assertThat(response).contains("Authentication required");
+	public void homeEndpointReturnsHello() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.content().string("Hello"));
 	}
 }
